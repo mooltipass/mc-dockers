e3722ef137141dd5bcd4064de4ca5f51f7621da1
limpkin <limpkin@limpkin.fr>
2020-03-23 15:33:07 +0100

    updated scripts

--- minible-win-builder/scripts/build.sh
+++ minible-win-builder/scripts/build.sh
@@ -5,9 +5,13 @@ set -ev
 PATH=$HOME/mxe/usr/bin:$PATH
 MXE_BASE=$HOME/mxe
 build_dir=/minible/build_win
+output_dir=/minible/output
 
+cd /minible
+git submodule update --init --recursive
 rm -fr $build_dir && mkdir -p $build_dir && cd $build_dir
 
-$MXE_BASE/usr/i686-w64-mingw32.shared.posix/qt5/bin/qmake ../minible_emu.pro
+$MXE_BASE/usr/i686-w64-mingw32.shared.posix/qt5/bin/qmake ../source_code/main_mcu/minible_emu.pro
 make -j$(nproc --all)
-
+mkdir -p $output_dir
+mv release/minible_emu.exe $output_dir
