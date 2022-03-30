5227171dc3924cd05cdd9d184d32113e30b41ca7
Raoul Hecky <raoul.hecky@gmail.com>
2022-03-30 13:57:32 +0200

    build with qt6.3.2

--- minible-win-builder/scripts/build.sh
+++ minible-win-builder/scripts/build.sh
@@ -15,7 +15,7 @@ git submodule update --init --recursive
 rm -fr $build_dir && mkdir -p $build_dir && cd $build_dir
 cp ../source_code/main_mcu/emu_assets/miniblebundle.img $output_dir
 
-$MXE_BASE/usr/i686-w64-mingw32.shared.posix/qt5/bin/qmake ../source_code/main_mcu/minible_emu.pro
+$MXE_BASE/usr/i686-w64-mingw32.shared.posix/qt6/bin/host-qmake ../source_code/main_mcu/minible_emu.pro
 make -j$(nproc --all)
 
 mv release/minible_emu.exe $output_dir
